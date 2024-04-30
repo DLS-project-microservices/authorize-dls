c75f4a129ec5f1ac105008110db093f89a7af31a
masp1234 <mart2c04@stud.kea.dk>
2024-04-30 12:48:06 +0200

    first commit

---
+++ authorize.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+
+function authorize(role, options = {}) {
+    const {
+        cookieName = '_auth',
+        secret = process.env.AUTH_SECRET,
+        rolePropertyName = 'userType',
+    } = options;
+
+    if (!secret) {
+        throw new Error('No secret provided. Add AUTH_SECRET to .env or provide it in the options parameter.')
+    }
+
+    return async (req, res, next) => {
+        const token = req.cookies[cookieName];
+        if (token) {
+            const claims = jwt.verify(token, secret)
+            if (claims[rolePropertyName] && claims[rolePropertyName] === role) {
+                req.userClaims = claims;
+                return next();
+            }
+        }
+        res.status(401).send({ message: `You are unauthorized. You must be role '${role}'.`})
+    }
+}     
+
+export default authorize;
